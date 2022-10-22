0189158d261619d9ee1666a378f668a210553497
akhayrullina <introspot129@gmail.com>
2022-10-22 14:40:40 +0300

    Задание Больше деталей. Нужно написать функцию, которая создает массив из 25 сгенерированных объектов.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,52 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
-/**
- * @param {number} min Нижняя граница диапазона
- * @param {number} max Верхняя граница диапазона
- * @returns {number} Случайное в заданном промежутке
- */
-function getRandomInteger (min, max) {
-  if (min < 0 || max < 0 || min >= max) {
-    return('Число должно быть положительным или равно нулю. Минимальное значенние не может быть больше максимального.')
+function getRandomPositiveInteger (a, b) {
+  if (a < 0 || b < 0) {
+    return NaN;
   }
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
 //Функция для проверки максимальной длины строки
-/**
- * @param {string} text Входная строка
- * @param {number} maxLength Максимальная длина
- * @returns {boolean} Подходит ли по длине
- */
-function getStringLength (text, maxLength) {
-  if (text.length <= maxLength) {
+function checkStringLength (string, length) {
+  if (string.length <= length) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
+
+//Функция, возвращая случайный элемент массива (описание к фотографии)
+const DESCRIPTION = [
+  'Делайте в вашей жизни то, что меньше заставляет вас смотреть в свой телефон.',
+  'Улыбка — единственный тренд в моде, который актуален всегда.',
+  'Никогда не ищите свое счастье там, где вы его однажды потеряли.',
+  'Жизнь похожа на фотокамеру: вам просто нужно смотреть на нее с улыбкой.',
+  'Моя жизнь меняется, потому что меняю ее я.',
+  'Всегда начинайте свой день с хороших людей и кофе.',
+  'Ни о чем не беспокойтесь. Потому что все лучшие умы на работе.',
+  'Жизнь — это всего лишь серия крошечных чудес, поэтому обратите внимание на них.',
+  'Живите во всех тех моментах, которые вы не можете выразить словами.'
+];
+
+function getRandomArrayItem (elements) {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+}
+
+//Функция для создания массива из 25 сгенерированных объектов
+function createPhoto (_, index) {
+  const id = index + 1;
+
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: getRandomArrayItem (DESCRIPTION),
+    likes: getRandomPositiveInteger (15, 200),
+    comments: getRandomPositiveInteger (0, 200)
+  };
+}
+
+const arrayPhotos = Array.from({length: 25}, createPhoto);
+console.log(arrayPhotos);
+checkStringLength ('', 140);
